8663ffd09e276784a45dc71681663e8194f3f3e0
Victor <fedorenko22116@gmail.com>
2018-05-17 05:10:54 +0300

    added profiler

--- config/bundles.php
+++ config/bundles.php
@@ -26,4 +26,5 @@ return [
     Sonata\ClassificationBundle\SonataClassificationBundle::class => ['all' => true],
     Sonata\NewsBundle\SonataNewsBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
