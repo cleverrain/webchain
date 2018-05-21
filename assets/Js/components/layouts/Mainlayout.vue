9eeb31e3dd4e4957e237ad660561d291f1dbf6f1
Khaled Alam <khaledalam.net@gmail.com>
2018-05-21 20:06:11 +0200

    [Footer] Add social media icons. Refs #4236

--- assets/Js/components/layouts/Mainlayout.vue
+++ assets/Js/components/layouts/Mainlayout.vue
@@ -5,6 +5,33 @@
         </div>
         <footer class="container-fluid mt-3 py-4 px-5">
             <div class="row text-center">
+
+                <div class="col-12 text-center">
+                    <p class="d-block align-items-center justify-content-center">
+                        <a href="https://www.facebook.com/coinimp" class="mr-3" rel="nofollow" target="_blank">
+                            <img :src="facebook" :width="50">
+                        </a>
+                        <a href="http://www.linkedin.com/company/webchain-blockchain" class="mr-3" rel="nofollow" target="_blank">
+                            <img :src="linkedin" :width="50">
+                        </a>
+                        <a href="https://www.twitter.com/coinimp" class="mr-3" rel="nofollow" target="_blank">
+                            <img :src="twitter" :width="50">
+                        </a>
+                        <a href="https://www.reddit.com/r/coinimp" class="mr-3" rel="nofollow" target="_blank">
+                            <img :src="reddit" :width="50">
+                        </a>
+                        <a href="http://www.t.me/joinchat/IdMikBC1Ew28ufxKdfnKMQ" class="mr-3" rel="nofollow" target="_blank">
+                            <img :src="telegram_channel" :width="50">
+                        </a>
+                        <a href="http://www.t.me/webchainnetwork" class="mr-3" rel="nofollow" target="_blank">
+                            <img :src="telegram_group" :width="50">
+                        </a>
+                        <a href="http://www.discordapp.com/invite/K7AptZt" class="mr-3" rel="nofollow" target="_blank">
+                            <img :src="discord" :width="50">
+                        </a>
+                    </p>
+                </div>
+
                 <div class="col-12">
                     <a class="p-0 pr-2" href="mailto:support@webchain.network">Contact Us</a>
                 </div>
@@ -17,7 +44,26 @@
 </template>
 
 <script>
+    import facebook from "../../../Images/social/icon-facebook.png"
+    import linkedin from "../../../Images/social/icon-linkedin.png"
+    import twitter from "../../../Images/social/icon-twitter.png"
+    import reddit from "../../../Images/social/icon-reddit.png"
+    import telegram_channel from "../../../Images/social/icon-telegram.png"
+    import telegram_group from "../../../Images/social/icon-telegram-group.png"
+    import discord from "../../../Images/social/icon-discord.png"
+
     export default {
-        name: "MainLayout"
+        name: "MainLayout",
+        data: function () {
+            return {
+                facebook: facebook,
+                linkedin: linkedin,
+                twitter: twitter,
+                reddit: reddit,
+                telegram_channel: telegram_channel,
+                telegram_group: telegram_group,
+                discord: discord
+            }
+        }
     }
 </script>
