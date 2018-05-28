bb34031d2d7e8ba21c414d6140532b48c04381c4
Khaled Alam <khaledalam.net@gmail.com>
2018-05-28 23:47:03 +0200

    [How to Start] Fix images sizes, use bootstrap class, use Annotation. Refs #4256

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,14 +7,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    /**
+     * Only for dev purposes. In prod environment
+     * it must be overritten by server
+     * @Route("/", name="index")
+     */
     public function index() :RedirectResponse
     {
         return $this->redirect('/build/static/index.html');
     }
 
+    /**
+     * @Route("/start", name="start")
+     */
     public function howToStartAction() :Response
     {
         return $this->render('how_to_start.html.twig');
