f7d09b2f95adfb509cb9feda94f2af9d7ccb0f67
Khaled Alam <khaledalam.net@gmail.com>
2018-06-05 14:15:08 +0200

    [FAQ] Move data from php class to template. Refs #4272

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,8 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Faq;
-use Symfony\Component\Asset\Packages;
 
 class DefaultController extends AbstractController
 {
@@ -23,15 +21,12 @@ class DefaultController extends AbstractController
     }
 
     /**
-     * @param Packages $assetPackage
      * @return Response
      * @Route("/faq", name="faq")
      */
-    public function faqAction(Packages $assetPackage) :Response
+    public function faqAction() :Response
     {
-        return $this->render('faq.html.twig', [
-            'data' => (new Faq($assetPackage))->getData()
-        ]);
+        return $this->render('faq.html.twig');
     }
 
     /**
