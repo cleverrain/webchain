cffbfeba1d812a0876bc548a80773051f9036983
Khaled Alam <khaledalam.net@gmail.com>
2018-06-09 19:36:27 +0200

    [Homepage] fix ratio coinimp section. Refs #4301

--- assets/Js/components/pages/Index.vue
+++ assets/Js/components/pages/Index.vue
@@ -317,78 +317,79 @@
         background-size: 100% calc(100% - 1vh)
 
         @include media-breakpoint-up(xxl)
-            padding-top: 3.5rem
-            height: 32rem
+            padding-top: 7.5rem
+            height: 31rem
             h2
-                font-size: 3.4em
+                font-size: 2.8em
             p
-                font-size: 2.9em
+                font-size: 1.8em
 
         @include media-breakpoint-down(xl)
             @include media-orientation(landscape)
-                height: 22.5rem
-                padding-top: 2rem
-                h2
-                    font-size: 2.7em
-                p
-                    font-size: 2em
-            @include media-orientation(portrait)
-                height: 25rem
+                height: 22rem
+                padding-top: 5rem
                 h2
                     font-size: 3.2em
                 p
-                    font-size: 2.5em
+                    font-size: 1.2em
+            @include media-orientation(portrait)
+                height: 24rem
+                h2
+                    font-size: 3em
+                p
+                    font-size: 1.8em
 
         @include media-breakpoint-down(lg)
             @include media-orientation(landscape)
-                height: 18.5rem
-                padding-top: 2rem
+                height: 18rem
+                padding-top: 4.5rem
+                h2
+                    font-size: 2em
+                p
+                    font-size: 1em
+            @include media-orientation(portrait)
+                height: 20rem
                 h2
                     font-size: 2.2em
                 p
-                    font-size: 1.4em
-            @include media-orientation(portrait)
-                height: 20.5rem
-                h2
-                    font-size: 2.35em
-                p
-                    font-size: 1.65em
+                    font-size: 1.5em
 
         @include media-breakpoint-down(md)
             @include media-orientation(landscape)
-                height: 14.5rem
-                padding-top: 1.5rem
+                height: 14rem
+                padding-top: 2.5rem
+                h2
+                    font-size: 1.7em
+                p
+                    font-size: 1em
+            @include media-orientation(portrait)
+                height: 16.4rem
                 h2
                     font-size: 1.9em
                 p
-                    font-size: 1.4em
-            @include media-orientation(portrait)
-                height: 16.5rem
-                h2
-                    font-size: 2.1em
-                p
-                    font-size: 1.6em
+                    font-size: 1.3em
 
         @include media-breakpoint-down(xxxs)
-            height: 10.5rem
+            height: 10rem
             h2
-                font-size: 1.4em
+                font-size: 1.2em
             p
-                font-size: 0.9em
+                font-size: 0.7em
 
         @include media-breakpoint-down(xxs)
             @include media-orientation(landscape)
-                height: 6.5rem
+                height: 6rem
                 h2
-                    font-size: 0.9em
-                p
-                    font-size: 0.4em
-            @include media-orientation(portrait)
-                height: 8.5rem
-                h2
-                    font-size: 1.25em
-                p
                     font-size: 0.7em
+                p
+                    font-size: 0.2em
+            @include media-orientation(portrait)
+                height: 8.4rem
+                padding-top: 2.2rem
+                h2
+                    font-size: 1.2em
+                p
+                    font-size: 0.5em
 
     #divisions
         
