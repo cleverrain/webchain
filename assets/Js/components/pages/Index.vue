e0d28a8021704c9b3e177c4b615610bec3dbdf22
Victor <fedorenko22116@gmail.com>
2018-06-16 17:40:14 +0300

    deleted gitter sidecar module. Refs #4313

--- assets/Js/components/pages/Index.vue
+++ assets/Js/components/pages/Index.vue
@@ -222,9 +222,6 @@
     import Division from '../Division';
     import TwtrTimeline from '../TwtrTimeline';
     import FeaturedButton from '../FeaturedButton';
-    import Sidecar from '../../modules/GitterSidecar';
-
-    Sidecar('webchain-network/public');
 
     export default {
         name: 'index',
